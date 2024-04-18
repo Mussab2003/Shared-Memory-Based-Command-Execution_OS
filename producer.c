b322973408a3da23ab00f9a9ecce272b168ae678
Mussab2003 <116267339+Mussab2003@users.noreply.github.com>
2024-04-18 21:05:44 +0000

    V2 -Multithreaded reading from txt file

--- producer.c
+++ producer.c
@@ -6,48 +6,72 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <unistd.h>
+#include <pthread.h>
+
+
+#define num_threads 4
+
+const int SHM_SIZE = 4096;
+const char *SHM_NAME = "OS";
+
+FILE *fptr;
+char buffer[5000];
+int shm_fd;
+char *ptr;
+
+void openFile()
+{
+    fptr = fopen("commands.txt", "r");
+    if(fptr == NULL)
+    {
+        printf("Error in opening file (commands.txt)\n");
+    }
+}
+    
+void openSHM()
+{
+    shm_fd = shm_open(SHM_NAME,O_CREAT | O_RDWR,0666);
+    ftruncate(shm_fd, SHM_SIZE);
+    ptr = (char *)mmap(0, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+}
+
+void *readFromFile(void *args)
+{
+    int thread_id = *((int *) args);
+    int start = thread_id * (100 / num_threads);
+    int end = start + (100 / num_threads);
+
+    for(int i=start; i<end; i++)
+    {
+        fgets(buffer, sizeof(buffer), fptr);
+        printf("%s", buffer);
+        sprintf(ptr, "%s\n", buffer);
+        ptr += strlen(buffer);
+    }
+
+}
 
 int main()
 {
+    openFile();
+    openSHM();
+    
+    pthread_t threads[num_threads];
+    int thread_args[num_threads];
 
-    const int SHM_SIZE = 4096;
-    const char *SHM_NAME = "OS";
-    ssize_t bytes_read;
-    char buffer[5000];
-
-    int shm_fd;
-    int file_fd;
-
-    file_fd = open("commands.txt", O_RDONLY);
-
-    if(file_fd ==-1)
+    for(int i=0; i<num_threads; i++)
     {
-        perror("Unable to open the file");
-        exit(1);
+        thread_args[i] = i;
+        pthread_create(&threads[i], NULL, readFromFile, (void *)&thread_args[i]);
     }
 
-    char *ptr;
+    for(int i=0; i<num_threads; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
 
-    shm_fd = shm_open(SHM_NAME,O_CREAT | O_RDWR,0666);
-
-    ftruncate(shm_fd, SHM_SIZE);
-
-    ptr = (char *)mmap(0, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-
-    // while ((bytes_read = read(file_fd, buffer, sizeof(buffer))) > 0)
-    // {
-    //     printf("%s", buffer);
-    // }
-
-    read(file_fd, buffer, sizeof(buffer));
-    sprintf(ptr,"%s \n", buffer);
-    
     close(shm_fd);
-    
-    if(close(file_fd) == -1)
-    {
-        perror("Unable to close file");
-        exit(1);
-    }
+    fclose(fptr);
+
     return 0;
 }
